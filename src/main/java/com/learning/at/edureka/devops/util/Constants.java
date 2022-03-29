a2e9e71da4653d451ec9183cf785a88470308b6a
Anuj Pankaj <anujpankaj@gmail.com>
2022-03-29 08:19:31 +0000

    added util class

---
+++ src/main/java/com/learning/at/edureka/devops/util/Constants.java
@@ -0,0 +1,9 @@
+package com.learning.at.edureka.devops.util;
+
+public class Constants {
+	
+	public static class Common{
+		
+		private static String GIT_HUB_URL = "https://github.com/anuj-pankaj/devops";
+	}
+}
